52887cbe4ee2d49c05c6327436f3858703f9efef
Gloria Johnston <92404130+IT2GO-Gloria@users.noreply.github.com>
2021-12-07 19:51:36 -0500

    Create CampingWebSite.js

---
+++ CampingWebSite.js
@@ -0,0 +1,6 @@
+// My Camp Web Pages on CodePen:
+// https://codepen.io/MomLion/pen/gOwjJyv ... My Home Page
+// https://codepen.io/MomLion/pen/JjRMozM ... My Details & Price page
+// https://codepen.io/MomLion/pen/NWRwzpr ... My Camp Booking Page
+// https://codepen.io/MomLion/pen/RwGxrLy ... My Book Confirmation Page
+//  ... My Reviews Page
